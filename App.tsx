4d5ae031ef60716a678318ba555286da5627c912
salmonco <xkqjsslsek80@naver.com>
2023-07-29 21:08:22 +0900

    Design: navigation background color

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import store from './src/store';
 import AppInner from './AppInner';
 
+const navTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#FEFDFF',
+  },
+};
+
 function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
+      <NavigationContainer theme={navTheme}>
         <AppInner />
       </NavigationContainer>
     </Provider>
