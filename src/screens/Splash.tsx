b57444bbe3ee22f510566cb490d384b16d3960e9
salmonco <xkqjsslsek80@naver.com>
2023-11-17 22:24:20 +0900

    Design: 스플래시 추가

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Logo from '../assets/images/logo.svg';
 
 function Splash() {
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.container}>
+      <Logo />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+});
 
 export default Splash;
